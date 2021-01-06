243c2754ad349092271874b6722c5528608ee913
mirun <mirun@LAPTOP-JL51469R>
2021-01-06 08:31:41 +0200

    Add project files.

---
+++ Bostan_Miruna_Lab12/Bostan_Miruna_Lab12/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Bostan_Miruna_Lab12.Data;
+
+namespace Bostan_Miruna_Lab12
+{
+    public partial class App : Application
+    {
+        public static ShoppingListDatabase Database { get; private set; }
+        public App()
+        {
+            Database = new ShoppingListDatabase(new RestService());
+
+            MainPage = new NavigationPage(new ListEntryPage());
+
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
